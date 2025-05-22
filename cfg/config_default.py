1ceb098256a354ae2ece11c3e432f732680d6cb8
Y <hertuno@yandex.ru>
2025-05-22 16:37:41 +0500

    first

---
+++ cfg/config_default.py
@@ -0,0 +1,4 @@
+#Для использования необходимо переименовать в config.py
+API_URL = "https://archive-api.open-meteo.com/v1/archive"
+COORDINATES = {"latitude": 12.3456, "longitude": 12.3456}
+DATA_PATH = {"raw": "data/raw/", "parsed": "data/parsed/"}
