cbc7f26fed741b647ecf17c363e11ea7dba44c08
Y <hertuno@yandex.ru>
2025-07-18 21:47:51 +0500

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+WEATHER_URL = "https://historical-forecast-api.open-meteo.com/v1/forecast"
+CALENDAR_URL = "https://calendar.kuzyak.in/api/calendar/"
+COORDINATES = {"latitude": 66.0833, "longitude": 76.6333}
+DATA_PATH = {"raw": "datasets/raw/", "parsed": "datasets/parsed/"}
